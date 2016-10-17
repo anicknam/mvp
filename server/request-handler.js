4dba8397365ad45301cb9461905640d440139611
anicknam <a.nicknam@gmail.com>
2016-10-17 15:08:27 -0700

    Finish Implement Mongoose

--- server/request-handler.js
+++ server/request-handler.js
@@ -3,43 +3,39 @@ var Movie = require('./db/models/Movie');
 
 exports.fetchMovies = function(req, res) {
 
-	// Movie.find(function(err, movies){
- //      if (err) {
- //      	res.status(500).send(err)
- //      } else {
- //      	res.status(200).send(movies);
- //      }
-	// });
+	Movie.find(function(err, movies){
+      if (err) {
+      	res.status(500).send(err)
+      } else {
+      	res.status(200).send(movies);
+      }
+	});
 
-	res.send('Inside get handler');
 };
 
 exports.saveMovie = function(req, res) {
-	// var title = req.body.title;
-	// var year = req.body.year;
-	// var genre = req.body.genre;
-	// var watched = req.body.watched || false;
+	var title = req.body.title;
+	var year = req.body.year;
+	var genre = req.body.genre;
+	var watched = req.body.watched || false;
 
- //    var newMovieEntry = new Movie({
- //    	title: title,
- //    	year: year,
- //    	genre: genre,
- //    	watched: watched
- //    });
+    var newMovieEntry = new Movie({
+    	title: title,
+    	year: year,
+    	genre: genre,
+    	watched: watched
+    });
     
-   // res.send(newMovieEntry);
 
-    // newMovieEntry.save(function(err, newMovieEntry){
-    // 	if (err) {
-    // 		res.status(500).send(err);
-    // 	} else {
-    // 		res.set({
-    // 			'access-control-allow-origin': '*'
-    // 		});
-    // 		res.status(200).send(newMovieEntry);
-    // 	}
-    // })
-
-  res.send('Inside post handler')
+    newMovieEntry.save(function(err, newMovieEntry){
+    	if (err) {
+    		res.status(500).send(err);
+    	} else {
+    		res.set({
+    			'access-control-allow-origin': '*'
+    		});
+    		res.status(200).send(newMovieEntry);
+    	}
+    })
 
 };
