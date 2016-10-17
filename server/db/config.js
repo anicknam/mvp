a483917c7e0a1c8d9988e7d0b4addf4c4155c866
anicknam <a.nicknam@gmail.com>
2016-10-17 14:54:00 -0700

    Implement Mongoose WIP

--- server/db/config.js
+++ server/db/config.js
@@ -1,12 +1,13 @@
 var mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost/movielog');
+mongoose.connect('mongodb://localhost/movieLog');
 
 var db = mongoose.connection;
 
-
-
-
+db.on('error', console.error.bind(console, 'Error connecting'))
+db.once('open', function(){
+	console.log('MonogDB connection open...');
+});
 
 
 module.exports = db;
--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var partials = require('express-partials');
 var path = require('path');
+var handler = require('./request-handler');
 
 var app = express();
 
@@ -9,9 +10,13 @@ app.use(partials());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-// app.get('/', function(req, res) {
-//   res.send('HELLO');
-// });
+
+
+app.use('/', handler.fetchMovies);
+app.post('/', handler.saveMovie);
+
+
+
 
 app.set('port', 8080);
 console.log('MovieLog listening on port: ', app.get('port'))
